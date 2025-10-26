8ca1c02c8ef4acece5fe1213944a17eb0604d083
Chetan Solanki <151900157+ChetaN7895@users.noreply.github.com>
2025-10-26 22:37:11 +0530

    Update vite.config.js

--- client-quick-ai/vite.config.js
+++ client-quick-ai/vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
-import  react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [ react(),
-    tailwindcss(),
-  ],
+  base: './', // ✅ crucial for correct asset loading in production
+  plugins: [react(), tailwindcss()],
 })
